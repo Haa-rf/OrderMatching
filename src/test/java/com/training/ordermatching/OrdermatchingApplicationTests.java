f051df3336cdb89d585341efd326bdf2d6d59907
Rolf <1779356953@qq.com>
2019-08-09 20:17:40 +0800

    init

---
+++ src/test/java/com/training/ordermatching/OrdermatchingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.training.ordermatching;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrdermatchingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
