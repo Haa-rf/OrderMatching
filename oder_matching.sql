8a8feb732df77987d10e65e9b9dc6a0b9bedef77
Rolf <1779356953@qq.com>
2019-08-11 22:20:40 +0800

    fix some problem

--- oder_matching.sql
+++ oder_matching.sql
@@ -1,4 +1,4 @@
-//User table
+--User table
 create table user(
    user_id INT NOT NULL AUTO_INCREMENT,
    user_name VARCHAR(128) NOT NULL,
@@ -7,24 +7,24 @@ create table user(
    PRIMARY KEY ( user_id )
 );
 
-//Order table
+--Order table
 create table orders(
   order_id INT NOT NULL AUTO_INCREMENT,
   symbol VARCHAR(128) NOT NULL,
   order_type VARCHAR(128) NOT NULL,
   side VARCHAR(128) NOT NULL,
-  quantity INT NOT NULL AUTO_INCREMENT,
-  price FLOAT NOT NULL,         //The length of float data type？
-  status VARCHAR(128) NOT NULL,
-  create_date DATE NOT NULL,
-  finish_date DATE NOT NULL,
+  quantity INT NOT NULL,
+  price FLOAT NOT NULL,         --The length of float data type？
+  status VARCHAR(128),
+  create_date DATE,
+  finish_date DATE,
   PRIMARY KEY (order_id)
  );
 
-//Trader_Order table
+--TraderOrder table
 create table trader_order(
-  user_id INT NOT NULL AUTO_INCREMENT,
-  order_id INT NOT NULL AUTO_INCREMENT
+  user_id INT NOT NULL,
+  order_id INT NOT NULL
 );
 
 
